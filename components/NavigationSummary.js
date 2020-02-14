54a90974b433963dbc185fde4dd107861167a687
kevinlabre <kevinlabreckc@gmail.com>
2020-02-14 13:41:45 +0100

    feat(step 2): WIP, css, img, ...

--- components/NavigationSummary.js
+++ components/NavigationSummary.js
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem, } from '@react-navigation/drawer';
 import Summary from './Summary';
 
 const Drawer = createDrawerNavigator();
 
+function CustomDrawerContent(props) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+        </DrawerContentScrollView>
+    );
+}
+
+
 function NavigationSummary() {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator drawerContent={props => CustomDrawerContent(props)}>
             <Drawer.Screen name="Summary" component={Summary} />
         </Drawer.Navigator>
     );
--- components/Summary.js
+++ components/Summary.js
@@ -8,30 +8,64 @@
 
 import React from 'react';
 
-import {StyleSheet, View, Text, Image, Button, ImageBackground, TouchableHighlight} from 'react-native';
+import {StyleSheet, View, Text, Image, Button, ImageBackground, TouchableHighlight, TouchableOpacity} from 'react-native';
 import stylesApp, {colors} from '../Style';
 
 class Summary extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
 
     _onPressButton() {
         // TODO
     }
 
+    _openDrawerNavigation = () => {
+        this.props.navigation.openDrawer();
+    }
+
     render() {
         return (
             <ImageBackground style={styles.view} source={require('./images/background.png')} resizeMode='cover'>
-                <Text style={[styles.white, styles.name]}>Alberta</Text>
+                <View style={styles.container_header}>
+                    <View style={styles.header_part_1}>
+                        <View>
+                            <TouchableOpacity onPress={this._openDrawerNavigation}>
+                                <Image resizeMode='contain' style={styles.menu} source={require('./images/menu.png')}/>
+                            </TouchableOpacity>
+                            </View>
+
+                        <Text style={[styles.white, styles.name]}>Alberta</Text>
+                    </View>
+                    <View>
+                        <Image resizeMode='contain' style={styles.options} source={require('./images/options.png')}/>
+                    </View>
+                </View>
+
                 <View style={styles.flex}>
                     <View style={styles.container_img}>
                         <Image resizeMode='contain' style={styles.tmp_img} source={require('./images/cloud.png')}/>
                     </View>
 
-                    <View style={styles.container_text}>
+                    <View style={styles.container_center}>
                         <Text style={[styles.white, styles.text, styles.tmp]}>
                             -7°
                         </Text>
-                        <Text style={[styles.white, styles.text]}>Foggy weather</Text>
-                        <Text style={[styles.white, styles.text]}>68°/56 <Image source={''}/> 0%</Text>
+
+                        <Text style={[styles.white, styles.text, styles.line_height]}>Foggy weather</Text>
+
+                            <Text style={[styles.white, styles.text]}>
+                                <Text style={styles.text_variable_tmp}>
+                                    68°/56
+                                </Text>
+
+                                <Image style={styles.tint_img} resizeMode='contain' source={require('./images/tint.png')}/>
+                                0%
+                            </Text>
+                    </View>
+
+                    <View style={styles.container_img}>
                     </View>
                 </View>
 
@@ -56,20 +90,26 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.primaryColorConst,
     },
-    container_img : {
-        flex: 1
+    container_img: {
+        flex: 1,
     },
-    container_text : {
-        flex : 2
+    container_center: {
+        // backgroundColor : 'red',
+        flex: 2,
     },
     flex: {
+        padding: 30,
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'flex-start',
-        alignItems: 'flex-start'
+        alignItems: 'flex-start',
+    },
+    line_height : {
+      lineHeight : 30
     },
     name: {
-        fontFamily: 'Roboto-Bold',
+        fontFamily: 'Roboto',
+        fontSize: 20,
     },
     white: {
         color: '#FFF',
@@ -77,20 +117,53 @@ const styles = StyleSheet.create({
     text: {
         fontFamily: 'Roboto',
         textAlign: 'center',
+        fontSize: 20,
     },
     tmp: {
-        fontSize: 150,
+        fontSize: 130,
         textAlign: 'center',
     },
     container_btn: {
         textAlign: 'right',
         flex: 1,
+        justifyContent : 'flex-end',
         alignItems: 'flex-end',
         marginRight: 50,
+        marginBottom: 50,
     },
     tmp_img: {
-        width: 100,
-        height: 100,
+        width: 70,
+        height: 70,
+    },
+    menu: {
+        width: 20,
+        height: 20,
+        paddingLeft: 70,
+    },
+    options: {
+        width: 20,
+        height: 20,
+        paddingRight: 50,
+    },
+    header_part_1: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    text_variable_tmp : {
+      // paddingRight: 10
+    },
+    tint_img: {
+        width: 15,
+        height: 15,
+    },
+    container_header: {
+        height: 70,
+        // flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     btn: {
         backgroundColor: colors.primaryGradientColorEndConst,
