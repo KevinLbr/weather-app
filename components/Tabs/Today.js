c6c1b342011fb194fcc9dc8664d723518944675b
kevinlabre <kevinlabreckc@gmail.com>
2020-02-20 09:23:59 +0100

    feat(step today tab): WIP

--- components/Tabs/Today.js
+++ components/Tabs/Today.js
@@ -9,6 +9,10 @@ import {
     Image,
 } from 'react-native';
 
+import Hr from 'react-native-hr-component';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faInfoCircle} from '@fortawesome/free-solid-svg-icons';
+
 import stylesApp, {colors} from '../../Style';
 
 export default class Today extends Component {
@@ -17,10 +21,16 @@ export default class Today extends Component {
 
         this.state = {
             globale_data: [
-                {key: 'noon', name: 'Noon', img: require('.|./images/cloud.png'), temp: '-8°C'},
+                {key: 'noon', name: 'Noon', img: require('../images/cloud.png'), temp: '-8°C'},
                 {key: 'morning', name: 'Morning', img: require('../images/cloud-showers-heavy.png'), temp: '-4°C'},
                 {key: 'day', name: 'Day', img: require('../images/cloud-sun-rain.png'), temp: '-3°C'},
                 {key: 'evening', name: 'Evening', img: require('../images/cloud-sun.png'), temp: '-5°C'},
+            ],
+            hourly_data: [
+                {key: 5, name: '5 PM', img: require('../images/cloud.png'), temp: '-8°C'},
+                {key: 6, name: '6 PM', img: require('../images/cloud-showers-heavy.png'), temp: '-5°C'},
+                {key: 7, name: '7 PM', img: require('../images/cloud-sun-rain.png'), temp: '-4°C'},
+                {key: 8, name: '8 PM', img: require('../images/cloud-sun.png'), temp: '-4°C'},
             ],
         };
     }
@@ -28,7 +38,7 @@ export default class Today extends Component {
     render() {
         return (
             <View style={styles.view}>
-                <FlatList data={this.state.globale_data}
+                <FlatList data={this.state.globale_data} style={styles.flat_list}
                           renderItem={({item}) => (
                               <View style={styles.view_list}>
                                   <Text style={styles.item_list_name}>{item.name}</Text>
@@ -36,12 +46,98 @@ export default class Today extends Component {
                                   <Text style={styles.item_list_temp}>{item.temp}</Text>
                               </View>)}
                 />
+
+                <View style={styles.container_expended_all}>
+                    <Hr lineColor="rgba(255,255,255,0.15)" width={1} textPadding={0} text=" "/>
+
+                    <Text style={styles.title_extended}>EXTENDED FORECAST</Text>
+
+                    <View style={{height: 70}}>
+                        <View style={styles.container_extended}>
+                            <FontAwesomeIcon icon={faInfoCircle} color={'#FFF'} style={styles.info}/>
+                            <Text style={styles.text_extended}>Expect rainy weather Sunday evening through late monday
+                                night</Text>
+                        </View>
+                    </View>
+                    <Hr lineColor="rgba(255,255,255,0.15)" width={1} textPadding={0} text=" "/>
+                </View>
+
+                <View style={styles.container_hourly}>
+                    <Text style={styles.title_hourly}>Hourly</Text>
+
+                    <FlatList data={this.state.hourly_data} contentContainerStyle={styles.hourly_flat_list}
+                              renderItem={({item}) => (
+                                  <View style={styles.container_hourly_flat_list}>
+                                      <Text style={styles.item_hourly_list_temp}>{item.name}</Text>
+
+                                      <View style={styles.container_temp_hourly_list}>
+                                          <Image resizeMode='contain' style={styles.item_hourly_list_img}
+                                                 source={item.img}/>
+                                          <Text style={styles.item_list_temp}>{item.temp}</Text>
+                                      </View>
+                                  </View>)}
+                    />
+                </View>
             </View>
         );
     }
 }
 
 let styles = StyleSheet.create({
+    container_hourly_flat_list: {
+        flex: 1,
+        margin: 10,
+        flexDirection: 'row',
+    },
+    item_hourly_list_img: {
+        height: 20,
+        width: 20,
+    },
+    container_temp_hourly_list: {
+
+    },
+    item_hourly_list_temp: {
+        color: 'rgba(255,255,255,0.65)',
+    },
+    title_hourly: {
+        fontSize: 20,
+        fontFamily: 'Roboto',
+        color: '#FFF',
+    },
+    container_hourly: {
+        flex: 10,
+    },
+    container_expended_all: {
+        flex: 7,
+    },
+    hourly_flat_list: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    container_extended: {
+        padding: 10,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+    info: {
+        marginRight: 10,
+        marginTop: 5,
+    },
+    text_extended: {
+        fontSize: 16,
+        color: 'rgba(255,255,255,0.65)',
+        fontFamily: 'Roboto',
+    },
+    title_extended: {
+        marginVertical: 5,
+        fontSize: 20,
+        textAlign: 'center',
+        color: colors.primaryGradientColorEndConst,
+        fontFamily: 'Roboto',
+    },
     item_list_img: {
         maxWidth: 40,
         maxHeight: 40,
@@ -69,6 +165,7 @@ let styles = StyleSheet.create({
     },
     view: {
         padding: 20,
+        flex: 1,
     },
     text: {
         color: 'white',
