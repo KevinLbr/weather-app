75308b62bb668cdd5fb977ba461f1ac9d49d650e
kevinlabre <kevinlabreckc@gmail.com>
2020-03-10 13:11:33 +0100

    feat(precipitation) : part 1 & 2

--- components/Tabs/Radar.js
+++ components/Tabs/Radar.js
@@ -8,30 +8,75 @@ import {
 } from 'react-native';
 
 import stylesApp, {colors} from '../../Style';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default class Radar extends Component {
     constructor(props) {
         super(props);
-
     }
 
-
     render() {
-        return (
-            <View style={styles.view}>
-                <Text style={styles.text}>Radar</Text>
-            </View>
-        );
+        if(this.props.bottomComponentIsOpen == true){
+            return (
+                <View style={styles.view}>
+                    <View style={styles.container}>
+                        <MapView
+                            style={styles.map}
+                            initialRegion={{
+                                latitude: 37.78825,
+                                longitude: -122.4324,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+
+                        >
+                        </MapView>
+                    </View>
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.view}>
+                    <View style={styles.container}>
+                        <MapView
+                            style={styles.map}
+                            initialRegion={{
+                                latitude: 37.78825,
+                                longitude: -122.4324,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+                        />
+                    </View>
+                </View>
+            );
+        }
     }
 }
 
 let styles = StyleSheet.create({
     view : {
         flex : 1,
-        justifyContent : 'center',
-        alignItems : 'center',
+        // justifyContent : 'center',
+        // alignItems : 'center',
     } ,
     text : {
         color : 'white',
-    }
+    },
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
 });
